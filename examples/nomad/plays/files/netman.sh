0c1187afc7f374ec5c18208cec55ea439af3b4a1
Carlos León <mail@carlosleon.info>
2016-07-01 17:53:56 +0200

    Update network management script
    * Making it less aggressive with the logging as log files were growing
      rather quickly.
    * Sleeping in between loops for two seconds to avoid performance hit.

--- examples/nomad/plays/files/netman.sh
+++ examples/nomad/plays/files/netman.sh
@@ -1,40 +1,30 @@
 #!/usr/bin/env bash
-
 function connect {
-  DONE=no
   name=$1
   ancestor=$2
   networks=$3
 
-  while [ $DONE == "no" ]
-  do
-    CID=$(docker ps -q -f ancestor=$ancestor)
-    if [ $CID ]
-    then
-      # Go over the networks that we wish the container is connected to
-      for network in $networks
-      do
-        # Get list of containers connected to $network
-        CIDS=$(docker network inspect -f "{{.Containers}}" $network)
-        if [[ $CIDS =~ $CID ]] # If the container id is present in the network's container list
-        then
-          echo "==> $name container already connected to $network network"
-        else
-          docker network connect $network $CID # connect container $CID to network $network
-          echo "==> $name container successfully connected to network $network"
-        fi
-      done
-
-      DONE=yes
-
-      echo "==> $name container connected to networks"
-    else
-      echo "* $name container not yet available"
-    fi
-  done
-
-  echo "==> done"
+  CID=$(docker ps -q -f ancestor=$ancestor)
+  if [ $CID ]
+  then
+    # Go over the networks that we wish the container is connected to
+    for network in $networks
+    do
+      # Get list of containers connected to $network
+      CIDS=$(docker network inspect -f "{{.Containers}}" $network)
+      if [[ ! $CIDS =~ $CID ]] # If the container is not yet in the list of containers connected to the network
+      then
+        # Connect container $CID to network $network
+        docker network connect $network $CID
+        echo "==> $name container successfully connected to network $network"
+      fi
+    done
+  fi
 }
 
-connect front-end weaveworksdemos/front-end "secure internal external"
-connect orders weaveworksdemos/orders "internal secure backoffice"
+while true
+do
+  connect front-end weaveworksdemos/front-end "secure internal external"
+  connect orders weaveworksdemos/orders "internal secure backoffice"
+  sleep 2
+done
